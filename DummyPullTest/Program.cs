6cfcb5e2459b573a756f69a12feca3e2797f7cb7
GonzoDark <Gonzo.Dark@gmail.com>
2017-10-05 08:45:33 +0200

    initial commit

---
+++ DummyPullTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DummyPullTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
